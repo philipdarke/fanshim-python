3b14f1d40fbf6765143116750a2694f014e0fdf3
Philip Darke <43066442+philipdarke@users.noreply.github.com>
2021-10-10 11:43:34 +0100

    Update FanShim class and add APA-102 class

--- library/fanshim/__init__.py
+++ library/fanshim/__init__.py
@@ -1,167 +1,106 @@
-import RPi.GPIO as GPIO
+import lgpio as gpio
 import time
-import apa102
-import atexit
-from threading import Thread
 
-__version__ = '0.0.5'
+__version__ = '1.0.0'
+
+
+class APA102():
+    def __init__(self, sbc, gpio_clock, gpio_data, brightness):
+        """APA102 LED class.
+        
+        Based heavily on https://github.com/pimoroni/apa102-python.
+        """
+        self.sbc = sbc
+        self.gpio_clock = gpio_clock
+        self.gpio_data = gpio_data
+        # Intialise buffer (see https://cpldcpu.wordpress.com/2014/08/27/apa102/)
+        self._start_length = 4
+        _frame_length = 4
+        _end_length = 4
+        self.buffer = self.reset_buffer(self._start_length, _frame_length, _end_length, brightness)
+
+    def reset_buffer(self, start_length, frame_length, end_length, brightness):
+        """Clear buffer i.e. LED off."""
+        buffer = []
+        for _ in range(start_length):
+            buffer.append(0b00000000)
+        buffer += [0b11100000 | int(brightness * 31) for _ in range(frame_length)]
+        for _ in range(end_length):
+            buffer.append(0b11111111)
+        return buffer
+    
+    def set_colour(self, r, g, b):
+        """Set LED colour.
+
+        :param r: amount of red (0-255).
+        :param g: amount of green (0-255).
+        :param b: amount of blue (0-255).
+        """
+        offset = self._start_length  + 1
+        self.buffer[offset:offset + 3] = [b, g, r]
+
+    def set_brightness(self, brightness):
+        """Set LED brightness.
+
+        :param brightness: LED brightness (0.0-1.0).
+        """
+        self.buffer[self._start_length] = 0b11100000 | int(31 * brightness)
+    
+    def write_buffer(self):
+        """Update LED state."""
+        for byte in self.buffer:
+            for _ in range(8):
+                gpio.gpio_write(self.sbc, self.gpio_data, (byte & 0x80))
+                gpio.gpio_write(self.sbc, self.gpio_clock, 1)
+                time.sleep(0)
+                byte <<= 1
+                gpio.gpio_write(self.sbc, self.gpio_clock, 0)
+                time.sleep(0)
 
 
 class FanShim():
-    def __init__(self, pin_fancontrol=18, pin_button=17, button_poll_delay=0.05, disable_button=False, disable_led=False):
+    def __init__(self, led_clock=14, led_data=15, fan_pin=18, led_brightness=0.05):
         """FAN Shim.
 
-        :param pin_fancontrol: BCM pin for fan on/off
-        :param pin_button: BCM pin for button
+        Based heavily on https://github.com/pimoroni/fanshim-python.
 
+        :param led_clock: SPI clock pin for APA102 LED.
+        :param led_data: SPI data pin for APA102 LED.
+        :param fan_pin: BCM pin for fan on/off.
+        :param led_brightness: Control brightness of LED (0-1 but not linear).
         """
-        self._pin_fancontrol = pin_fancontrol
-        self._pin_button = pin_button
-        self._poll_delay = button_poll_delay
-        self._button_press_handler = None
-        self._button_release_handler = None
-        self._button_hold_handler = None
-        self._button_hold_time = 2.0
-        self._t_poll = None
-
-        self._disable_button = disable_button
-        self._disable_led = disable_led
-
-        GPIO.setwarnings(False)
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self._pin_fancontrol, GPIO.OUT)
-
-        if not self._disable_button:
-            GPIO.setup(self._pin_button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-        if not self._disable_led:
-            self._led = apa102.APA102(1, 15, 14, None, brightness=0.05)
-
-        atexit.register(self._cleanup)
-
-    def start_polling(self):
-        """Start button polling."""
-        if self._disable_button:
-            return
-
-        if self._t_poll is None:
-            self._t_poll = Thread(target=self._run)
-            self._t_poll.daemon = True
-            self._t_poll.start()
-
-    def stop_polling(self):
-        """Stop button polling."""
-        if self._disable_button:
-            return
-
-        if self._t_poll is not None:
-            self._running = False
-            self._t_poll.join()
-
-    def on_press(self, handler=None):
-        """Attach function to button press event."""
-        def attach_handler(handler):
-            self._button_press_handler = handler
-            self.start_polling()
-
-        if handler is not None:
-            attach_handler(handler)
-        else:
-            return attach_handler
-
-    def on_release(self, handler=None):
-        """Attach function to button release event."""
-        def attach_handler(handler):
-            self._button_release_handler = handler
-            self.start_polling()
-
-        if handler is not None:
-            attach_handler(handler)
-        else:
-            return attach_handler
-
-    def on_hold(self, handler=None):
-        """Attach function to button hold event."""
-        def attach_handler(handler):
-            self._button_hold_handler = handler
-            self.start_polling()
-
-        if handler is not None:
-            attach_handler(handler)
-        else:
-            return attach_handler
-
-    def set_hold_time(self, hold_time):
-        """Set the button hold time in seconds.
-
-        :param hold_time: Amount of time button must be held to trigger on_hold (in seconds)
-
-        """
-        self._button_hold_time = hold_time
+        self._led_clock = led_clock
+        self._led_data = led_data
+        self._fan_pin = fan_pin
+        # Raspberry Pi GPIO class
+        self._sbc = gpio.gpiochip_open(0)
+        gpio.gpio_write(self._sbc, fan_pin, 1)  # fan is on by default
+        # LED class
+        self._led = APA102(self._sbc, led_clock, led_data, led_brightness)
 
     def get_fan(self):
         """Get current fan state."""
-        return GPIO.input(self._pin_fancontrol)
+        return gpio.gpio_read(self._sbc, self._fan_pin)
+
+    def set_fan(self, state):
+        """Set fan state."""
+        gpio.gpio_write(self._sbc, self._fan_pin, state)
+        return state
 
     def toggle_fan(self):
         """Toggle fan state."""
-        return self.set_fan(False if self.get_fan() else True)
-
-    def set_fan(self, fan_state):
-        """Set the fan on/off.
-
-        :param fan_state: True/False for on/off
-
-        """
-        GPIO.output(self._pin_fancontrol, True if fan_state else False)
-        return True if fan_state else False
+        fan_state = self.get_fan()
+        gpio.gpio_write(self._sbc, self._fan_pin, not fan_state)
+        return not fan_state
 
     def set_light(self, r, g, b, brightness=None):
         """Set LED.
 
-        :param r: Red (0-255)
-        :param g: Green (0-255)
-        :param b: Blue (0-255)
-
+        :param r: Red (0-255).
+        :param g: Green (0-255).
+        :param b: Blue (0-255).
         """
-        if self._disable_led:
-            return
-
-        self._led.set_pixel(0, r, g, b)
+        self._led.set_colour(r, g, b)
         if brightness is not None:
-            self._led.set_brightness(0, brightness)
-        self._led.show()
-
-    def _cleanup(self):
-        self.stop_polling()
-
-        self.set_light(0, 0, 0)
-
-    def _run(self):
-        self._running = True
-        last = 1
-
-        while self._running:
-            current = GPIO.input(self._pin_button)
-            # Transition from 1 to 0
-            if last > current:
-                self._t_pressed = time.time()
-                self._hold_fired = False
-
-                if callable(self._button_press_handler):
-                    self._t_repeat = time.time()
-                    Thread(target=self._button_press_handler).start()
-
-            if last < current:
-                if callable(self._button_release_handler):
-                    Thread(target=self._button_release_handler, args=(self._hold_fired,)).start()
-
-            if current == 0:
-                if not self._hold_fired and (time.time() - self._t_pressed) > self._button_hold_time:
-                    if callable(self._button_hold_handler):
-                        Thread(target=self._button_hold_handler).start()
-                    self._hold_fired = True
-
-            last = current
-
-            time.sleep(self._poll_delay)
+            self._led.set_brightness(brightness)
+        self._led.write_buffer()
